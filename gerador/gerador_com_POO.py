5c4651933173c155bbee05e03308acbee106be0a
Victor Nogueira <victorjose.nogueirasantos@hotmail.com>
2023-01-31 16:17:41 -0300

    Update gerador_com_POO.py

--- gerador/gerador_com_POO.py
+++ gerador/gerador_com_POO.py
@@ -1,9 +1,12 @@
+import random
 import tkinter
 from tkinter import *
 from tkinter import messagebox
-from random import randint, choice
+import random
 import os
 import pyperclip
+import string
+
 arquivo = os.path.dirname(__file__)
 nome_Do_arquivo_criado = arquivo+'\\gerador_de_senhas_automatico.txt'
 
@@ -12,27 +15,45 @@ class janela:
 
     def __init__(self, titulo='', lxa=''):
         self.dicionario = []
+        # interface
         self.janela = Tk()
         self.janela.title(titulo)
         self.janela.geometry(lxa)
+
+        #escritas
         self.escrita = Label(self.janela, text='Gerador de senhas', anchor=N, background='#d3d3d3', font=('Arial', 15))
         self.escrita.place(x=50, y=10, height=30)
+
+        #escrita2
         self.escrita2 = Label(self.janela, text='Site:', anchor=W, background='#d3d3d3')
         self.escrita2.place(x=10, y=70, width=100, height=20)
+
+        #caixas de texto
         self.caixadetexto = Entry(self.janela)
         self.caixadetexto.place(anchor=W, x=55, y=80, width=200, height=20)
+
+        #escritas3
         self.escrita3 = Label(self.janela, text='Log-in:', anchor=W, background='#d3d3d3')
         self.escrita3.place(x=10, y=100, width=100, height=20)
+
+        #caixa de texto 2
         self.caixadetexto2 = Entry(self.janela)
         self.caixadetexto2.place(anchor=W, x=55, y=110, width=200, height=20)
+
+        #check button
         self.checkbutton = IntVar()
         self.check = tkinter.Checkbutton(self.janela, text='site permite caracteres especiais ?', variable=self.checkbutton)
         self.check.place(x=10, y=130)
+
+        #botoes
         self.botao1 = self.botoes(self.janela,text='Gerar Senha', anchor=N, comando=self.senhar,x=10, y=170, width=100, height=30)
         self.botao2 = self.botoes(self.janela, text='Salvar Senha', anchor=N, comando=self.combinandofunc, x=10,y=200,width=100, height=30)
         self.botao3 = self.botoes(self.janela, text='mostrar senhas', anchor=N, comando=self.abridor, x=170, y=170, width=100, height=30)
         self.janela.config(background='#d3d3d3')
+
+        #fim da janela
         self.janela.mainloop()
+
     def salvarsenhas(self):
         bancodesenhas = open(nome_Do_arquivo_criado, 'a')
         bancodesenhas.write(f'site: ' + self.caixadetexto.get()+'\n')
@@ -66,54 +87,23 @@ class janela:
 
     def senhar(self):
         self.dicionario.clear()
+        tamanhos = 16
         lista = ['!', '@', '#', '$', '%', '&', '*']
         listaa = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'o', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                   'u',
                   'v', 'w', 'x', 'y', 'z']
         senha = []
         if self.checkbutton.get() == 1:
-            u = choice(lista)
-            d = choice(listaa)
-            t = choice(listaa)
-            q = choice(listaa)
-            g = choice(listaa)
-            s = choice(listaa)
-            se = randint(0, 9)
-            o = randint(0, 9)
-            senha.append(u)
-            senha.append(d)
-            senha.append(t)
-            senha.append(q)
-            senha.append(g)
-            senha.append(s)
-            var = str(se)
-            senha.append(var)
-            var1 = str(o)
-            senha.append(var1)
-            x = ''.join(senha)
+            chars = string.ascii_letters + string.digits + '!@#$%&*-_+=?|/¬'
+            rng = random.SystemRandom()
+            x = ''.join(rng.choice(chars) for i in range(tamanhos))
             self.dicionario.append(x)
             print(x)
             messagebox.showinfo('Senha gerada', f'{x}')
         else:
-            u = choice(listaa)
-            d = choice(listaa)
-            t = choice(listaa)
-            q = choice(listaa)
-            g = choice(listaa)
-            s = choice(listaa)
-            se = randint(0, 9)
-            o = randint(0, 9)
-            senha.append(u)
-            senha.append(d)
-            senha.append(t)
-            senha.append(q)
-            senha.append(g)
-            senha.append(s)
-            var = str(se)
-            senha.append(var)
-            var1 = str(o)
-            senha.append(var1)
-            x = ''.join(senha)
+            chars = string.ascii_letters + string.digits
+            rng = random.SystemRandom()
+            x = ''.join(rng.choice(chars) for i in range(tamanhos))
             self.dicionario.append(x)
             print(x)
             messagebox.showinfo('Senha gerada', f'{x}')
